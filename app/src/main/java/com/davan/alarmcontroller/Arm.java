9bde2a5f695167410988c33928e4672a966f44c2
davandev <davan_at_work@hotmail.com>
2017-06-10 00:14:12 +0200

    Smaller cleanup

--- app/src/main/java/com/davan/alarmcontroller/Arm.java
+++ app/src/main/java/com/davan/alarmcontroller/Arm.java
@@ -4,12 +4,6 @@ package com.davan.alarmcontroller;
  **/
 import android.content.Intent;
 import android.graphics.Color;
-import android.media.AudioManager;
-import android.media.MediaPlayer;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
-import android.media.ToneGenerator;
-import android.net.Uri;
 import android.os.CountDownTimer;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
